26cbd7fdab6d252972d4ae627cb6571efb0b709c
Daniel Garnier-Moiroux <git@garnier.wf>
2021-01-02 01:38:40 +0100

    Add perimetre, refactor stuff

--- quadrilatere.py
+++ quadrilatere.py
@@ -1,22 +1,46 @@
+from math import sqrt
+
+
 class Quadrilatere:
+
     def __init__(self, s1, s2, s3, s4):
-        self.x1, self.y1 = s1
-        self.x2, self.y2 = s2
-        self.x3, self.y3 = s3
-        self.x4, self.y4 = s4
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+        self.s4 = s4
 
     def __repr__(self):
         return (f"Quadrilatere ["
-                f"({self.x1}, {self.y1}), "
-                f"({self.x2}, {self.y2}), "
-                f"({self.x3}, {self.y3}), "
-                f"({self.x4}, {self.y4})]")
+                f"{self.s1}, "
+                f"{self.s2}, "
+                f"{self.s3}, "
+                f"{self.s4}]")
 
     def aire(self):
+        x1, y1 = self.s1
+        x2, y2 = self.s2
+        x3, y3 = self.s3
+        x4, y4 = self.s4
+        # Ne fonctionne que pour les quadrilateres convexes, oups
+        return (abs((x1 - x3) * (y2 - y4)) + abs((x2 - x4) * (y1 - y3))) / 2
+
+    def perimetre(self):
+        return sum(self.__longueur(segment)
+                   for segment in self.__segments())
+
+    def __segments(self):
         return (
-            abs((self.x1 - self.x3) * (self.y2 - self.y4))
-            + abs((self.x2 - self.x4) * (self.y1 - self.y3))
-        ) / 2
+            (self.s1, self.s2),
+            (self.s2, self.s3),
+            (self.s3, self.s4),
+            (self.s4, self.s1)
+        )
+
+    def __longueur(_, segment):
+        s1, s2 = segment
+        x1, y1 = s1
+        x2, y2 = s2
+        return sqrt((x1-x2)**2 + (y1-y2)**2)
 
 
 if __name__ == "__main__":
@@ -27,4 +51,6 @@ if __name__ == "__main__":
         (5, 4)
     )
     print("q:", q)
+    print("Périmètre de q:", q.perimetre())
     print("Aire de q:", q.aire())
+    print("~~~~~~~~~~~~~~~")
